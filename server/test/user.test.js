00119836b1ec73595c36d0f38f73e9f9e3d96ccb
Onengiye <klevamane@gmail.com>
2018-03-21 07:47:13 +0100

    Test API to create a new user

---
+++ server/test/user.test.js
@@ -0,0 +1,28 @@
+import chaiHttp from 'chai-http';
+import chai from 'chai';
+import app from '../app';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+// DELETE
+describe('POST /user', () => {
+  it('should create a new user', (done) => {
+    const user = {
+      id: 1,
+      email: 'userone@email.com',
+      firstname: 'Durant',
+      lastname: 'Kevin',
+      password: 'newpassword',
+      createdAt: '03/03/2018',
+      updatedAt: '03/03/2018'
+    };
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
