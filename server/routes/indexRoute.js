afe0a5e64116d873f27df35bc0609b08abd82757
klevamane <31253707+klevamane@users.noreply.github.com>
2018-03-22 14:19:52 +0100

    Revert "Test api get businesses 155683186"

--- server/routes/indexRoute.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import usercontroller from '../controller/userController';
-
-const routes = express.Router();
-routes.get('/signdown', usercontroller.getUserById);
-
-export default routes;
