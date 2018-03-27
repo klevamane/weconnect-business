b0d8e460831507cd17172b73ea440743e8eda0a0
Onengiye <klevamane@gmail.com>
2018-03-27 10:08:33 +0100

    Add business test

--- server/routes/businessRoute.js
+++ server/routes/businessRoute.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import businesscontroller from '../controller/businessController';
 import reviewController from '../controller/reviewController';
+import { checkLocation, checkCategory, checkMobile, checkBusinessName } from '../helper/validation';
+
 
 const routes = express.Router();
 
-routes.post('/', businesscontroller.createBusiness);
+routes.post(
+  '/', checkBusinessName, checkCategory, checkLocation, checkMobile,
+  businesscontroller.createBusiness
+);
 routes.get('/', businesscontroller.getAllBusinesses);
 routes.get('/:businessId', businesscontroller.getBusinessById);
 routes.put('/:businessId', businesscontroller.updateBusiness);
